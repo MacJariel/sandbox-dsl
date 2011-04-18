dd8bf036292a613de4f817579dda497b783fc7dd
MacJariel <MacJariel@gmail.com>
2011-04-18 14:08:06 +0200

    Traceability model gets saved.

--- org.mcj.dsl.debugger.core/src/org/mcj/dsl/debugger/DSLDebuggerCore.java
+++ org.mcj.dsl.debugger.core/src/org/mcj/dsl/debugger/DSLDebuggerCore.java
@@ -1,11 +1,14 @@
 package org.mcj.dsl.debugger;
 
+import org.eclipse.acceleo.engine.event.IAcceleoTextGenerationListener;
+import org.eclipse.acceleo.engine.service.AcceleoService;
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.mcj.dsl.debugger.listeners.AcceleoGenerationListener;
 import org.osgi.framework.BundleContext;
 
 /**
- * The activator class controls the plug-in life cycle
+ * The activator class controls the plug-in life cycle.
  */
 public class DSLDebuggerCore extends AbstractUIPlugin {
 
@@ -14,7 +17,10 @@ public class DSLDebuggerCore extends AbstractUIPlugin {
 
 	// The shared instance
 	private static DSLDebuggerCore plugin;
-	
+
+	// The AcceleoTextGenerationListener
+	private IAcceleoTextGenerationListener acceleoTextGenerationListener = new AcceleoGenerationListener();
+
 	/**
 	 * The constructor
 	 */
@@ -23,25 +29,35 @@ public class DSLDebuggerCore extends AbstractUIPlugin {
 
 	/*
 	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
+	 * 
+	 * @see
+	 * org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext
+	 * )
 	 */
 	public void start(BundleContext context) throws Exception {
 		super.start(context);
 		plugin = this;
+
+		AcceleoService.addStaticListener(this.acceleoTextGenerationListener);
 	}
 
 	/*
 	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
+	 * 
+	 * @see
+	 * org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext
+	 * )
 	 */
 	public void stop(BundleContext context) throws Exception {
 		plugin = null;
 		super.stop(context);
+
+		AcceleoService.removeStaticListener(this.acceleoTextGenerationListener);
 	}
 
 	/**
 	 * Returns the shared instance
-	 *
+	 * 
 	 * @return the shared instance
 	 */
 	public static DSLDebuggerCore getDefault() {
@@ -49,10 +65,11 @@ public class DSLDebuggerCore extends AbstractUIPlugin {
 	}
 
 	/**
-	 * Returns an image descriptor for the image file at the given
-	 * plug-in relative path
-	 *
-	 * @param path the path
+	 * Returns an image descriptor for the image file at the given plug-in
+	 * relative path
+	 * 
+	 * @param path
+	 *            the path
 	 * @return the image descriptor
 	 */
 	public static ImageDescriptor getImageDescriptor(String path) {
